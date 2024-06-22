f4b0ad22118e1da1f7133c57c2e3923115f3b554
Tran Gia Khanh <giakhanhcnh@gmail.com>
2024-06-22 21:08:51 +0700

    Rewrite testgeninp

--- testgeninp.cpp
+++ testgeninp.cpp
@@ -1,57 +1,56 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
-#include <iomanip>
-#include <string>
-#include <direct.h> // Windows specific
+#include<bits/stdc++.h>
+//#define int long long
+#define speed ios_base::sync_with_stdio(0); cin.tie(0);
 
-void generate_test_case(const std::string& input_filename, int N) {
-    std::ofstream input_file(input_filename);
+#define fi first
+#define se second
+#define taskname ""
+using namespace std;
+const int maxn=1e5+10;
+const int mod=1e9+7;
 
-    // Sinh ngẫu nhiên mảng N số
-    std::vector<int> array(N);
-    for (int i = 0; i < N; ++i) {
-        array[i] = rand() % 10000001; // Các số trong mảng <= 10^7
-    }
+namespace fs = std::filesystem;
 
-    // Ghi vào file input
-    input_file << N << "\n";
-    for (int num : array) {
-        input_file << num << " ";
-    }
+void generate_test_case(const std::string& input_filename, const std::string& output_filename, int N) {
+    ofstream input_file(input_filename);
+    ofstream output_file(output_filename);
+    //Your code to generate test case here
     input_file << "\n";
 
+
     input_file.close();
+    output_file.close();
 }
 
 int main() {
     srand(time(0));
     int num_tests;
     int max_N;
-    std::string folder_name;
+    string folder_name;
+    cout << "Number of test cases:";
+    cin >> num_tests;
+    cout << "Max value of N:";
+    cin >> max_N;
+    cout << "Enter file name:";
+    cin>>folder_name;
+    
+    fs::create_directory(folder_name);
 
-    std::cout << "Nhap so luong test cases: ";
-    std::cin >> num_tests;
-    std::cout << "Nhap gia tri N toi da: ";
-    std::cin >> max_N;
-    std::cout << "Nhap ten thu muc lon: ";
-    std::cin >> folder_name;
-
-    // Tạo thư mục lớn
-    _mkdir(folder_name.c_str());
-
-    for (int i = 1; i <= num_tests; ++i) {
+    for (int i = 1; i <= num_tests; i++) {
         int N = rand() % max_N + 1;
-        std::ostringstream input_filename, subfolder;
+        std::ostringstream input_filename, output_filename, subfolder;
 
         subfolder << folder_name << "/test" << std::setw(3) << std::setfill('0') << i;
-        _mkdir(subfolder.str().c_str());
+        fs::create_directory(subfolder.str());
+        //if you only gen input file and want to name it with test00x.inp and test00x.out (with x is the number of test case), you can use this code
+        // input_filename << subfolder.str() << "/test" << std::setw(3) << std::setfill('0') << i << ".inp";
+        // output_filename << subfolder.str() << "/test" << std::setw(3) << std::setfill('0') << i << ".out";
 
-        input_filename << subfolder.str() << "/test" << std::setw(3) << std::setfill('0') << i << ".inp";
+        //this code will help you to sun your solution code to have correct output.
+        input_filename << subfolder.str() << "/" << folder_name << ".inp";
+        output_filename << subfolder.str() << "/" << folder_name << ".out";
 
-        generate_test_case(input_filename.str(), N);
+        generate_test_case(input_filename.str(), output_filename.str(), N);
     }
 
     return 0;
