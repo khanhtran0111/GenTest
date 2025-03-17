29791fc2f46a33302cbf868a96e999a0237b27a2
khanhtran0111 <giakhanhcnh@gmail.com>
2025-03-17 18:11:11 +0700

    clean

--- Gen/Gen.bat
+++
@@ -1,8 +0,0 @@
-setlocal enabledelayedexpansion
-set code=%~n0
-set count=0
-set testfol=test
-g++ -std=c++17 %code%.cpp -o %code%.exe
-for /D %%i in (*) do (
-%code% < %%~nxi\%code%.INP > %%~nxi\%code%.OUT
-)
